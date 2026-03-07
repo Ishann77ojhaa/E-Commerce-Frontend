a6813e6728c0aada5dddbb1a026ac7e0202a69f9
Ishann Ojhaa <ishannojhaa@gmail.com>
2026-03-07 15:31:26 +0545

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className="
+       text-lg ...
+       text-center
+      no-underline hover:underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
