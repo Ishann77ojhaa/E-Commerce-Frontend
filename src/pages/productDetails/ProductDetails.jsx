96afd93c82f40440b40e3aa00ee191631c715271
Ishann Ojhaa <ishannojhaa@gmail.com>
2026-09-07 01:41:43 +0545

    prod

--- src/pages/productDetails/ProductDetails.jsx
+++ src/pages/productDetails/ProductDetails.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 
+import Sproduct from './components/product/Sproduct'
 import Review from './components/review/Review'
-import Sproduct from './components/product/sproduct'
+
 import { useParams } from 'react-router-dom'
 
 
